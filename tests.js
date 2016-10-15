97cce078650ba1e70d7186d1bd560fb0005a2952
Uyiosa <hiskonxeptz@gmail.com>
2016-10-15 13:27:25 +0100

    Added some more test suites for accuracy checks and special ints

--- tests.js
+++ tests.js
@@ -17,8 +17,45 @@ describe("Test that the Input type by user is Authentiated ", function() {
     it("It should not accept Strings", function() {
         assert(sumofprimes.sumofprimes("this is not a Prime no") == "Only Integers are accepted", "can use isinteger")
     })
+
+       it("It should not accept Decimal numbers", function() {
+        assert(sumofprimes.sumofprimes(15.3) == "Only Integers are accepted", "can use isinteger")
+    })
+
+})
+
+
+describe("Test if it handles special Integers like neg values etc ", function() {
+    it("it should output 0 for input of 0 and 1", function() {
+        assert(sumofprimes.sumofprimes(0) == 0, "0 isnt an Integers")
+        assert(sumofprimes.sumofprimes(1) == 0, "0 isnt an Integers")
+    }) 
+
+    it("It should return error message for negative values", function() {
+        assert(sumofprimes.sumofprimes(-1) == "Only Positive Integers are accepted" "use condition")
+    })
+
+    it("It should return error message for greater than highest known prime no", function() {
+        assert(sumofprimes.sumofprimes(2,147,483,647) == "Only Positive Integers are accepted" "use condition")
+    })
+
 })
 
 
 
+describe("Test if it returns correct answers and acomodates decimals such as 4.0", function() {
+    it("it should return 17 for 10 and 3087 for 175", function() {
+        assert(sumofprimes.sumofprimes(10) == 17)
+        assert(sumofprimes.sumofprimes(17) == 58)
+    }) 
+
+    it("It should return error message for negative values", function() {
+        assert(sumofprimes.sumofprimes(-1) == "Only Positive Integers are accepted" "use condition")
+    })
+
+    it("It should return error message for greater than highest known prime no", function() {
+        assert(sumofprimes.sumofprimes(2,147,483,647) == "Only Positive Integers are accepted" "use condition")
+    })
+
+})
   
